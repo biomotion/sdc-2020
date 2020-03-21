99752f61c77d032e67a806a4f30e2ebe90dad4bd
biomotion <bensonpuppy1997@gmail.com>
2020-03-21 13:57:12 +0800

    correct x11 forwarding

--- docker_run.sh
+++ docker_run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 XAUTH=/tmp/.docker.xauth
-if [ ! -f $XAUTH ]; then
+if [ ! -f $XAUTH ]
+then
     xauth_list=$(xauth nlist $DISPLAY)
-    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
-    if [ ! -z "$xauth_list" ]; then
+    xauth_list=$(sed -e 's/^..../ffff/' <<< "$xauth_list")
+    if [ ! -z "$xauth_list" ]
+    then
         echo "$xauth_list" | xauth -f $XAUTH nmerge -
     else
         touch $XAUTH
@@ -20,25 +22,23 @@ fi
 
 BASH_OPTION=bash
 if [ ! -z "$1" ]; then
-    BASH_OPTION=$1
+    BASH_OPTION=$@
 fi
 
-if [ ! -z "$2" ]; then
-    BASH_ARGS=$2
-fi
 
 echo "running $BASH_OPTION $BASH_ARGS in docker"
 
 docker run -it --rm \
-       --network host \
-       --privileged \
-       --name sdc-2020 \
-       -e DISPLAY=$DISPLAY \
-       -e ROS_MASTER_URI=$ROS_MASTER_URI \
-       -e ROS_IP=$ROS_IP \
-       -e QT_X11_NO_MITSHM=1 \
-       -v $HOME/.Xauthority:/root/.Xauthority \
-       -v /tmp/.X11-unix:/tmp/.X11-unix \
-       -e XAUTHORITY=$XAUTH \
-       -v /dev:/dev \
-       biomotion/sdc-2020:latest $BASH_OPTION
+        --network host \
+        --privileged \
+        --name sdc-2020 \
+        -e DISPLAY \
+        -e QT_X11_NO_MITSHM=1 \
+        -e XAUTHORITY=$XAUTH \
+        -v "$XAUTH:$XAUTH" \
+        -v "/tmp/.X11-unix:/tmp/.X11-unix" \
+        -e ROS_MASTER_URI=$ROS_MASTER_URI \
+        -e ROS_IP=$ROS_IP \
+        -v $HOME/.Xauthority:/root/.Xauthority \
+        -v /dev:/dev \
+        biomotion/sdc-2020:latest $BASH_OPTION
