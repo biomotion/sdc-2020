3681cb0fed1d73759a580342e29f529e4475692a
biomotion <bensonpuppy1997@gmail.com>
2020-03-21 11:09:52 +0800

    add xauth to docker run

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+XAUTH=/tmp/.docker.xauth
+if [ ! -f $XAUTH ]; then
+    xauth_list=$(xauth nlist $DISPLAY)
+    xauth_list=$(sed -e 's/^..../ffff/' <<<"$xauth_list")
+    if [ ! -z "$xauth_list" ]; then
+        echo "$xauth_list" | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+# Prevent executing "docker run" when xauth failed.
+if [ ! -f $XAUTH ]; then
+    echo "[$XAUTH] was not properly created. Exiting..."
+    exit 1
+fi
+
 BASH_OPTION=bash
 if [ ! -z "$1" ]; then
     BASH_OPTION=$1
@@ -12,12 +30,15 @@ fi
 echo "running $BASH_OPTION $BASH_ARGS in docker"
 
 docker run -it --rm \
-       --network=host \
+       --network host \
        --privileged \
        --name sdc-2020 \
        -e DISPLAY=$DISPLAY \
        -e ROS_MASTER_URI=$ROS_MASTER_URI \
        -e ROS_IP=$ROS_IP \
+       -e QT_X11_NO_MITSHM=1 \
        -v $HOME/.Xauthority:/root/.Xauthority \
+       -v /tmp/.X11-unix:/tmp/.X11-unix \
+       -e XAUTHORITY=$XAUTH \
        -v /dev:/dev \
        biomotion/sdc-2020:latest $BASH_OPTION
